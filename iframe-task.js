d7a25c7f286e253e8a31d97926ea727562f8e2ab
P05h <121305788+P05h@users.noreply.github.com>
2023-09-02 08:30:19 -0400

    Create iframe-task.js

---
+++ iframe-task.js
@@ -0,0 +1,20 @@
+window['%hammerhead%'].utils.removeInjectedScript();
+
+window['%hammerhead%'].start({
+    cookie: "",
+    sessionId: 'a790181d880a42ddb5654714cf4cd45c',
+    serviceMsgUrl: 'https://browser.rammerhead.org:443/messaging',
+    transportWorkerUrl: 'https://browser.rammerhead.org:443/transport-worker.js',
+    isFirstPageLoad: false,
+    crossDomainProxyPort: '443',
+    referer: "https://browser.rammerhead.org/a790181d880a42ddb5654714cf4cd45c!i!s*www.google.com/https://www.youtube.com/embed/?enablejsapi=1&rel=0&autoplay=0&playsinline=1&expflag=embeds_enable_muted_autoplay%3Atrue&fs=1",
+    forceProxySrcForImage: true,
+    iframeTaskScriptTemplate: "window['%hammerhead%'].utils.removeInjectedScript();\n\nwindow['%hammerhead%'].start({\n    cookie                       : {{{cookie}}},\n    sessionId                    : 'a790181d880a42ddb5654714cf4cd45c',\n    serviceMsgUrl                : 'https://browser.rammerhead.org:443/messaging',\n    transportWorkerUrl           : 'https://browser.rammerhead.org:443/transport-worker.js',\n    isFirstPageLoad              : false,\n    crossDomainProxyPort         : '443',\n    referer                      : {{{referer}}},\n    forceProxySrcForImage        : true,\n    iframeTaskScriptTemplate     : {{{iframeTaskScriptTemplate}}},\n    allowMultipleWindows         : true,\n    isRecordMode                 : false,\n    windowId                     : ''\n}, window);\n\n(function () {\n    \n})();\n",
+    allowMultipleWindows: true,
+    isRecordMode: false,
+    windowId: ''
+}, window);
+
+(function() {
+}
+)();
